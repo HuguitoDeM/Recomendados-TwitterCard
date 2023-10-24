96d66dca586d3229aab1de3e3d1b033aae28fab6
HuguitoDeM <hugojavierdesideriomatinez@gmail.com>
2023-10-24 13:00:01 -0300

    intentando hacer un pop up

--- src/Componentes/PopUpTwitter.jsx
+++ src/Componentes/PopUpTwitter.jsx
@@ -1,11 +1,17 @@
-export function PopUpTwitter(){
+// eslint-disable-next-line react/prop-types
+export function PopUpTwitter({usuario,nombre,img,siguiendo,text}){
 
 
 return(
 
-<div className="coso">
-
+<header>
+<div className="TwitterCard-div">
+    <img className="TwitterCard-img" src={img} alt="" />
+    <strong className="TwitterCard-div-h1" >{nombre}</strong>
+    <span className="TwitterCard-div-span">{usuario}</span>
 </div>
+<button className={siguiendo}>{text}</button>
+</header>
 )
 
 }
--- src/Componentes/TwitterCard.jsx
+++ src/Componentes/TwitterCard.jsx
@@ -1,41 +1,48 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import {PopUpTwitter} from './PopUpTwitter.jsx'
+
+
 export  function TwitterCard({usuario, nombre, siguiendo}){
     const [Isfollowing, setFollowing]= useState(siguiendo)
     const [IsUnfollowing, setUnFollowing]= useState("Siguiendo")
+const [isMouseInside, setIsMouseInside]= useState(false)
+
 
     let text = Isfollowing ? IsUnfollowing : "Seguir" ; 
     
  let clasesBotton = Isfollowing ?  "TwitterCard-button Isfollowing": "TwitterCard-button"; 
 
-
+let imagen= `https://unavatar.io/twitter/${usuario}`
 
 function handleclick(){
 setFollowing(!Isfollowing)
 }
 function handleEntermouse(){ 
         setUnFollowing("Dejar de seguir");
-     
+    
     }
 function handleLeavemouse(){
     setUnFollowing("Siguiendo")
-
 }    
 
 
 
     return(
  <>
- <PopUpTwitter/>
-<article className="TwitterCard" > 
-    <header className="TwitterCard-header">
-    <img className="TwitterCard-img" src={`https://unavatar.io/twitter/${usuario}`} alt="" />
-
- <div className="TwitterCard-div"><strong className="TwitterCard-div-h1">{nombre}</strong> 
  
- <span className="TwitterCard-div-span">@{usuario}</span></div>
- </header> 
+<article className="TwitterCard" > 
+<header className="TwitterCard-header"
+          onMouseEnter={() => setIsMouseInside(true)}   
+          onMouseLeave={() => setIsMouseInside(false)}
+        >
+          <img className="TwitterCard-img" src={imagen} alt="" />
+          <div className="TwitterCard-div">
+            <strong className="TwitterCard-div-h1">{nombre}</strong>
+            <span className="TwitterCard-div-span">@{usuario}</span>
+          </div>
+          {isMouseInside && <PopUpTwitter usuario={usuario} nombre={nombre} img={imagen} siguiendo={clasesBotton} text={text} />}
+        </header>
  <aside>
     <div className={clasesBotton} onClick={handleclick} onMouseLeave={handleLeavemouse} onMouseEnter={handleEntermouse}>{text}</div>
  </aside>
